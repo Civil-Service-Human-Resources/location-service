d90255cb7c0693d4106d3e3ee6a5e5a1e7b5080c
Gordon <gordon.mackay@valtech.co.uk>
2018-01-29 09:00:12 +0000

    Some integration tests.

--- location-service/src/test/java/uk/gov/cshr/locationservice/service/LocationServiceControllerTest.java
+++ location-service/src/test/java/uk/gov/cshr/locationservice/service/LocationServiceControllerTest.java
@@ -1,62 +1,106 @@
 package uk.gov.cshr.locationservice.service;
 
-import java.nio.charset.Charset;
-import static org.hamcrest.Matchers.is;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.http.MediaType;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.junit.Assert.assertTrue;
+import org.junit.Ignore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import uk.gov.cshr.locationservice.controller.LocationServiceController;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import uk.gov.cshr.locationservice.LocationServiceApplication;
+import uk.gov.cshr.locationservice.LocationServiceException;
+import uk.gov.cshr.locationservice.controller.Coordinates;
 
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = LocationServiceApplication.class)
-//@ContextConfiguration
-//@WebAppConfiguration
-@RunWith(MockitoJUnitRunner.class)
-public class LocationServiceControllerTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = LocationServiceApplication.class)
+@ContextConfiguration
+@WebAppConfiguration
+//@RunWith(MockitoJUnitRunner.class)
+public class LocationServiceControllerTest extends AbstractTestNGSpringContextTests {
 
-
-    @Mock
-    private MapITService mapITService;
-
-    @InjectMocks
-    private LocationServiceController locationServiceController;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
     private MockMvc mockMvc;
 
-    final private MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));
-
-    @Before
+    @BeforeMethod
     public void setup() {
-        MockitoAnnotations.initMocks(this);
-        this.mockMvc = MockMvcBuilders.standaloneSetup(locationServiceController).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
+    @Test
+    public void testAreaPrefix() throws Exception {
+
+        Coordinates coordinates = findCoordinates("BRISTO");
+
+        assertTrue("Latitude", coordinates.getLatitude().equals(51.4538601656348));
+        assertTrue("Longitude", coordinates.getLongitude().equals(-2.591766955851817));
+    }
 
     @Test
-    public void testFindLocation() throws Exception {
+    public void testPartialPostcode() throws Exception {
 
-        // Given
-        String path = "/findlocation/BRIS";
+        Coordinates coordinates = findCoordinates("BS1");
 
-        // When
+        assertTrue("Latitude", coordinates.getLatitude().equals(51.4538601656348));
+        assertTrue("Longitude", coordinates.getLongitude().equals(-2.591766955851817));
+    }
+
+    @Test
+    public void testFullPostcode() throws Exception {
+
+        Coordinates coordinates = findCoordinates("BS16JS");
+
+        assertTrue("Latitude", coordinates.getLatitude().equals(51.4538601656348));
+        assertTrue("Longitude", coordinates.getLongitude().equals(-2.591766955851817));
+    }
+
+    @Test
+    @Ignore
+    public void testTwoCharSearch() throws Exception {
+
+        try {
+            Coordinates coordinates = findCoordinates("BA");
+        }
+        catch (LocationServiceException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testNoResults() throws Exception {
+
+        ResultActions sendRequest = mockMvc.perform(get("/findlocation/xyz"));
+        sendRequest.andExpect(status().isNotFound());
+    }
+
+    Coordinates findCoordinates(String searchTerm) throws Exception {
+
+        String path = "/findlocation/" + searchTerm;
+
         ResultActions sendRequest = mockMvc.perform(get(path));
 
-        // Then
-        sendRequest
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.coordinates.latitude", is(1)))
-                .andExpect(jsonPath("$.coordinates.longitude", is(1)));
+        sendRequest.andExpect(status().isOk());
+        MvcResult mvcResult = sendRequest.andReturn();
+        String result = mvcResult.getResponse().getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode actualObj = mapper.readTree(result);
+
+        Coordinates coordinates = new Coordinates();
+        coordinates.setLatitude(actualObj.get("latitude").asDouble());
+        coordinates.setLongitude(actualObj.get("longitude").asDouble());
+
+        return coordinates;
     }
 }
