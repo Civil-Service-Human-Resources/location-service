752fe27617c7efeb4bcb503b71ce30c1b4c12daa
reachsaurabh <reachsaurabh@users.noreply.github.com>
2018-01-30 15:06:19 +0000

    Revert "Initial prototype"

--- location-service/src/main/java/uk/gov/cshr/locationservice/service/CoordinatesService.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.cshr.locationservice.service;
-
-import uk.gov.cshr.locationservice.LocationServiceException;
-import uk.gov.cshr.locationservice.controller.Coordinates;
-
-public interface CoordinatesService {
-
-    Coordinates findCoordinates(String searchTerm) throws LocationServiceException;
-}
