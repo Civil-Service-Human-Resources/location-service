a718e365203117eb1f9a33bbe0371600d32f6eff
Seamus Singh <35099822+seamussingh@users.noreply.github.com>
2018-04-13 09:18:00 +0100

    Remove commented out code.

--- location-service/src/main/java/uk/gov/cshr/locationservice/service/RegionLookup.java
+++ location-service/src/main/java/uk/gov/cshr/locationservice/service/RegionLookup.java
@@ -70,31 +70,6 @@ public class RegionLookup {
         return ukNutsMap;
     }
 
-//    private static void checkMemory() {
-//
-//        numberOfCoordinates++;
-//
-//        if ( numberOfCoordinates % 10000 == 0 ) {
-//
-//            System.out.println("numberOfCoordinates=" + numberOfCoordinates);
-//
-//            // Get current size of heap in bytes
-//            long heapSize = Runtime.getRuntime().totalMemory();
-//            System.out.println("heapSize=" + heapSize);
-//            // Get maximum size of heap in bytes. The heap cannot grow beyond this size.// Any attempt will result in an OutOfMemoryException.
-//            long heapMaxSize = Runtime.getRuntime().maxMemory();
-//            System.out.println("heapMaxSize=" + heapMaxSize);
-//             // Get amount of free memory within the heap in bytes. This size will increase // after garbage collection and decrease as new objects are created.
-//            long heapFreeSize = Runtime.getRuntime().freeMemory();
-//            System.out.println("heapFreeSize=" + heapFreeSize);
-//
-//            System.gc();
-//        }
-//
-//
-//
-//    }
-
     private static void readFeatures(Iterator<JsonNode> jsonNodeIterator, HashMap<UK_NUTS, List<Path2D.Double>> ukNutsMap) {
 
         JsonNode featureNode = jsonNodeIterator.next();
@@ -155,7 +130,6 @@ public class RegionLookup {
             else {
                 polygon.lineTo(latitude, longitude);
             }
-//            checkMemory();
         }
 
         return polygon;
