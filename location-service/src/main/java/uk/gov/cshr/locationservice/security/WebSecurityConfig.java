c26ef4a4143d454f8da583b48640186e151f7d75
Gordon <gordon.mackay@valtech.co.uk>
2018-04-16 10:12:56 +0100

    Added basic auth

---
+++ location-service/src/main/java/uk/gov/cshr/locationservice/security/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package uk.gov.cshr.locationservice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${spring.location.security.username}")
+    private String username;
+
+    @Value("${spring.location.security.password}")
+    private String password;
+
+    @Autowired
+	private AuthenticationEntryPoint authEntryPoint;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+				.anyRequest().authenticated()
+				.and().httpBasic()
+				.authenticationEntryPoint(authEntryPoint);
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+                .withUser(username)
+                .password(password)
+                .roles("USER");
+	}
+}
